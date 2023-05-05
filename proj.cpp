1f1b310073aa15e25bebf8e99b8dd8cc492aad33
Jalpatel23 <120700773+Jalpatel23@users.noreply.github.com>
2023-05-05 10:40:44 +0530

    Update proj.cpp

--- proj.cpp
+++ proj.cpp
@@ -4,18 +4,20 @@ class proj
 {
     int i = 0;
     string name = "yash";
-    char n;
+    string n;
 public:
     void guess();
     void getchar();
     void check();
-}
+    void error();
+};
 
 void proj::guess()
 {
-    while (name != '\0')
+    while (name[i] != '\0')
     {
         getchar();
+        check();
     }
     cout << "you guessed the word right, congrats!!\n\n\n\n";
 }
@@ -24,11 +26,12 @@ void proj::getchar()
 {
     cout << "enter letter " << i + 1 << ": ";
     cin >> n;
+    error();
 }
 
 void proj::check()
 {
-    if (n == name[i] || n == name[i - 32])
+    if (n[0] == name[i] || n[0] == name[i - 32])
     {
         cout << "\nletter " << i + 1 << " is correct" << endl;
         ++i;
@@ -36,6 +39,30 @@ void proj::check()
     else
     {
         cout << "wrong letter, guess the character " << i + 1 << " again: " << endl;
+    }
+}
+
+void proj::error()
+{
+    if (n[1] != '\0')
+    {
+        cout << "error!! you entered more than 1 character" << endl;
+        cout << "enter character again" << endl;
+        getchar();
+    }
+    else if (n[0] >= 'A' && n[0] <= 'Z')
+    {
+        cout << endl;
+    }
+    else if (n[0] >= 'a' && n[0] <= 'z')
+    {
+        cout << endl;
+    }
+    else
+    {
+        cout << "error!! you did not enter a character" << endl;
+        cout << "enter again" << endl;
+        getchar();
     }
 }
 
