28fe7bfbf1d3b1fe13b65fa0fdd9146f1c2113b1
yashpatel122 <131847554+yashpatel122@users.noreply.github.com>
2023-05-05 09:58:58 +0530

    Update proj.cpp

--- proj.cpp
+++ proj.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+class proj
+{
+    int i = 0;
+    string name = "jal";
+    char n;
+public:
+    void guess();
+    void getchar();
+    void checking();
+}
 
+void proj::guess()
+{
+    while (name != '\0')
+    {
+        getchar();
+    }
+    cout << "you guessed the word right, congrats!!\n\n\n\n";
+}
+
+void proj::getchar()
+{
+    cout << "enter letter " << i + 1 << ": ";
+    cin >> n;
+}
+
+int main()
+{
+    proj ob;
+    cout << "guess the person's name by typing characters of their name: " << endl;
+}
