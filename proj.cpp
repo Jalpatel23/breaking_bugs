3200bb28c3d05fa75c38179ba46cd40c58e2a821
aryanpatel4256 <131357204+aryanpatel4256@users.noreply.github.com>
2023-05-05 11:25:21 +0530

    Update proj.cpp

--- proj.cpp
+++ proj.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 class proj
 {
@@ -10,6 +11,7 @@ public:
     void getchar();
     void check();
     void error();
+    void setColor(int color);
 };
 
 void proj::guess()
@@ -19,11 +21,13 @@ void proj::guess()
         getchar();
         check();
     }
+    setColor(9);
     cout << "you guessed the word right, congrats!!\n\n\n\n";
 }
 
 void proj::getchar()
 {
+    setColor(9);
     cout << "enter letter " << i + 1 << ": ";
     cin >> n;
     error();
@@ -33,19 +37,23 @@ void proj::check()
 {
     if (n[0] == name[i] || n[0] == name[i - 32])
     {
+        setColor(10);
         cout << "\nletter " << i + 1 << " is correct" << endl;
         ++i;
     }
     else
     {
+        setColor(14);
         cout << "wrong letter, guess the character " << i + 1 << " again: " << endl;
     }
+    setColor(9);
 }
 
 void proj::error()
 {
     if (n[1] != '\0')
     {
+        setColor(12);
         cout << "error!! you entered more than 1 character" << endl;
         cout << "enter character again" << endl;
         getchar();
@@ -60,15 +68,24 @@ void proj::error()
     }
     else
     {
+        setColor(12);
         cout << "error!! you did not enter a character" << endl;
         cout << "enter again" << endl;
         getchar();
     }
+    setColor(9);
+}
+
+void proj::setColor(int color)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
 }
 
 int main()
 {
     proj ob;
+    ob.setColor(9);
     cout << "guess the person's name by typing characters of their name: " << endl;
     ob.guess();
 }
